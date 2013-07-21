5b1b8cefe616bf4e6f28fce8ce134f5bae6be183
Alex Musayev <alex.musayev@gmail.com>
2013-07-21 04:36:49 +0400

    Argument type/action misterious collision fixed.

--- pyke/pyke.py
+++ pyke/pyke.py
@@ -2,12 +2,11 @@
 
 import argparse
 import os
-import inspect
-from functools import partial
 import sys
 from pprint import pprint
 from . import const
 from . import helpers
+from . import version
 
 
 def parse(metadata, args, description=None):
@@ -16,27 +15,31 @@ def parse(metadata, args, description=None):
                                      description=description,
                                      epilog=const.EPILOG)
 
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest='command')
+
     for command in metadata:
         subparser = subparsers.add_parser(command['name'], help=command['help'])
         for arg in command['args']:
-            subparser.add_argument(*helpers.arg_names(arg),
-                                   default=arg['default'],
-                                   help=arg['help'])
+            if arg['type'] == bool:
+                subparser.add_argument(*helpers.arg_names(arg),
+                                       default=bool(arg['default']),
+                                       action='store_true',
+                                       help=arg['help'])
+            else:
+                subparser.add_argument(*helpers.arg_names(arg),
+                                       default=arg['default'],
+                                       type=arg['type'],
+                                       help=arg['help'])
 
-    # let's try this
-    parser.parse_args('cleanup --help'.split())
+    args = parser.parse_args(args)
 
-    args = {}
-    name = 'dummy_command'
-
-    return name, args
+    return args.command, vars(args)
 
 
 def main():
     pykefile = helpers.get_pykefile(os.getcwd())
     if not pykefile:
         exit('pykefile not found')
-    commands, metadata, description = helpers.load_commands(pykefile)
+    commands, metadata, description = helpers.load_pykefile(pykefile)
     name, args = parse(metadata, sys.argv[1:], description)
     commands[name](**args)
